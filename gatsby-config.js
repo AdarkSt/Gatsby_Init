8c3f27472a473715d694b743427f0b03621fc44f
AdarkSt <goqhak777@mail.ru>
2023-02-20 12:14:56 +0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `gatsby-intro`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
